1bc16f24d03a885fd51867bfa55de5206095fda0
Inna7777 <peshkova-inna@mail.ru>
2022-12-01 21:30:44 +0400

    add third task

---
+++ Task03/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число и
+// выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число ");
+string nStr = Console.ReadLine() ??"";
+int n = Convert.ToInt32(nStr);
+int nrem = n % 2;
+ if (nrem == 0)
+ {
+    Console.WriteLine (n+ " четное число");
+ }
+ else {
+    Console.WriteLine (n+ " не является четным числом");
+ }
